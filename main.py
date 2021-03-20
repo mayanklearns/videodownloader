91010b043b963bb983153caac2a8bb4f2bad6f43
Mayank Poddar <72143861+mayanklearns@users.noreply.github.com>
2021-03-20 19:41:57 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from pytube.cli import on_progress
 from flask import Flask, render_template, request, send_file, redirect
 
 app = Flask(__name__)
-def downloadvideo(url):
-    youtube = pytube.YouTube(url, on_progress_callback=on_progress)
-    video = youtube.streams.filter(progressive=True, res = "720p").first()
-    filename = video.title
-    filesizeinmb = video.filesize/1000000
-    fileurl = video.url
-    return fileurl
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/download", methods = ["GET","POST"])
+def download():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template('download.html')
     else:
         url = request.form["link"]
         youtube = pytube.YouTube(url, on_progress_callback=on_progress)
@@ -24,5 +21,6 @@ def index():
         fileurl = video.url
         return redirect(fileurl)
 
+
 if __name__ == "__main__":
-    app.run(host="firstytdownload.herokuapp.com")
+    app.run(debug=True)
